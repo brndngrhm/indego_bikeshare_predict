62ff6184cf2480a235b42b77a845c4d5a03b0eea
brndngrhm <brndngrhm@users.noreply.github.com>
2020-10-31 11:10:48 -0400

    data exploration doc

--- R/helper.R
+++ R/helper.R
@@ -3,16 +3,18 @@
 
 ##############################################
 
-get_trips_data <- function(){
+# more info on the creation of this in `data_explore.Rmd`
+
+get_trips_data <- function(data){
   
-  trips <- read_csv("data/daily_indego_bikeshare_trips.csv") %>%
+  trips <- data %>%
+    rename(trips = n) %>%
+    mutate(month = month(date, label = T, abbr = T),
+           day_nm = wday(date, label = T, abbr = T)) %>%
     clean_names() %>%
-    # remove time columns and `ozone`, `precip_accumulation` which have a lot of missing obs
-    select(-ends_with("time"), -c(ozone, precip_accumulation)) %>%
-    # remove dates with no weather data
-    filter(!(is.na(summary))) %>%
-    # assuming `NA` here means there was no precip that day
-    mutate(precip_type = ifelse(is.na(precip_type), "none", precip_type))
+    select(-ends_with(c("time", "min", "low")), starts_with("temperature"), -c(row_num, ozone, precip_accumulation, wind_bearing, wind_gust, moon_phase)) %>%
+    mutate(precip_type = ifelse(is.na(precip_type), "none", precip_type)) %>%
+    na.omit()
   
   return(trips)
   
